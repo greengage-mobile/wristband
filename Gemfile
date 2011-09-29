9859ba3a58351abd0bec77833b7e16fdf0c26104
Hesham E <elnahhas@gmail.com>
2011-09-29 17:45:20 -0400

    Removed dependencies

--- Gemfile
+++ Gemfile
@@ -1,8 +1,6 @@
 source 'http://rubygems.org'
 
 gem 'rails', '>=3.1.0'
-gem 'haml', '>=3.1.3'
-gem 'formatted_form', '>=1.0.2'
 
 group :development, :test do
   gem 'sqlite3'
--- app/helpers/form_helper.rb
+++
@@ -1,8 +0,0 @@
-module FormHelper  
-  def formatted_form_for(record_or_name_or_array, *args, &proc)
-    options = args.extract_options!
-    options.merge!(:builder => FormattedFormBuilder)
-    (options[:html] ||= { }).merge!(:class => "#{options[:html][:class]} formatted")
-    form_for(record_or_name_or_array, *(args << options), &proc)
-  end
-end
